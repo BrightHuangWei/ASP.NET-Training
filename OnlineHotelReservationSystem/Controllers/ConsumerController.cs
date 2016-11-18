a9fe6330a90c37b2f92dd36a22916f3b04ad7c83
HuangWei <acer@9599.com>
2016-11-19 00:04:28 +0800

    主页设计（datepicker未显示）

---
+++ OnlineHotelReservationSystem/Controllers/ConsumerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineHotelReservationSystem.Controllers
+{
+    public class ConsumerController : Controller
+    {
+        // GET: Consumer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Booking()
+        {
+            return View();
+        }
+    }
+}
